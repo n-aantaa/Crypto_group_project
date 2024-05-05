81db68650b092fbfeccb1b4f146608645ea98770
Anta Mbaye <antambaye@MacBook-Air-de-Anta.local>
2024-05-04 21:39:21 -0500

    RSA

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# import customtkinter
+import customtkinter
 import sys
 import logging
 
@@ -13,31 +13,41 @@ except ModuleNotFoundError:
 
 
 def RSA_encryption():
-    return
+    print()
+    n = int( input())
+    e= int( input())
+    x=int( input())
+    y=fast_raise_power_book(x, e, n)
+    return y
 
 
 def RSA_decryption():
-    return
+    d= int( input())
+    y= int( input())
+    n = int(input())
+    x= fast_raise_power_book(y,d,n)
+    return x
 
-# customtkinter.set_appearance_mode("light")
-# customtkinter.set_default_color_theme("dark-blue")
+
+customtkinter.set_appearance_mode("light")
+customtkinter.set_default_color_theme("dark-blue")
 
 
 
 
-# root = customtkinter.CTk()
-# root.geometry("500x500")
-#
-# def login():
-#     print("test")
-#
-# frame = customtkinter.CTkFrame(master=root)
-# frame.pack(pady=20, padx=60, fill="both", expand=True)
-#
-# label = customtkinter.CTkLabel(master=frame, text="Crypto Group Project", font=("Helvetica",24))
-# label.pack(pady=20, padx=30)
-#
-# button1 = customtkinter.CTkButton(master=frame, text="AES", font=("Helvetica",24))
-# button1.pack(pady=20, padx=20)
-#
-# root.mainloop()
+root = customtkinter.CTk()
+root.geometry("1024x1024")
+
+def login():
+    print("test")
+
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+label = customtkinter.CTkLabel(master=frame, text="Crypto Group Project", font=("Helvetica",24))
+label.pack(pady=20, padx=30)
+
+button1 = customtkinter.CTkButton(master=frame, text="AES", font=("Helvetica",24), command=login())
+button1.pack(pady=20, padx=20)
+
+root.mainloop()
